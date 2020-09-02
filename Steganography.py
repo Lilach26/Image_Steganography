8689805eb3acaccc7ee937a0954b16f61a6bf4b1
Lilach26 <55951495+Lilach26@users.noreply.github.com>
2020-09-02 16:26:26 +0300

    convert_int_2binary

--- Steganography.py
+++ Steganography.py
@@ -3,3 +3,33 @@ import cv2
 import random
 from PIL import Image
 
+
+def convert_int_2binary(text):
+    binary = [format(ord(value), '08b') for value in text]
+    return binary
+
+
+def main():
+    type = input("Enter the mode: 1.Text 2.Image: ")
+
+    if type == 1:
+        modeText = input("Want to 1. encode / 2. decode: ")
+
+        if modeText == 1:
+            text = input("Please input the text you want to encode: ")
+            encode()
+        else:
+            print("The hidden text is: " + decode())
+    elif type == 2:
+        modeImage = input("Want to 1.encode / 2.decode: ")
+        if modeImage == 1:
+            encodeImage()
+        else:
+            decodeImage()
+
+    else:
+        raise Exception("Invalid input!")
+
+
+if __name__ == "__main__":
+    main()
